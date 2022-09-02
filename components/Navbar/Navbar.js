6be2bc193f8826c7d4f7b6ba26eef27a37e5186a
abstractjazz <evan@heyevan.com>
2022-09-01 20:14:41 -0700

    adds nav in layout

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+const Navbar = ()=>{
+return(
+
+  <>
+    <header className="border-solid border-white border mx-28 mt-8">
+   <Image src="/dot_vonn_logo.png" alt="Dot Vonn Logo" width='100%' height='100%'/>
+    </header>
+  </>
+
+)
+
+}
+
+export default Navbar;
